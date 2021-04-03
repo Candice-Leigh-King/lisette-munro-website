c0332edabdab579a18b27d8a24d61a7334ec730e
Candice-Leigh-King <candyschofie@gmail.com>
2021-04-03 16:41:19 +0200

    s

--- _site/main.js
+++ _site/main.js
@@ -17,24 +17,3 @@ $(document).ready(function() {
 
 	});
 	// end hamburger menu
-
-// for slider
-	$('.slider-for').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  fade: true,
-  asNavFor: '.slider-nav'
-});
-});
-
-$('.slider-nav').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  asNavFor: '.slider-for',
-  dots: true,
-  centerMode: true,
-  focusOnSelect: true
-});
-
-});
