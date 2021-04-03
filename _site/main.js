017b18375c34751920919f299f4749cca2d9ab27
Candice-Leigh-King <candyschofie@gmail.com>
2021-04-03 13:57:50 +0200

    v

---
+++ _site/main.js
@@ -0,0 +1,40 @@
+
+$(document).ready(function() {
+	//make sure the page is ready befor running the scripts
+	// This is the hamburger menu
+	$(".hamburger").click(function(){
+
+	  $(this).toggleClass('active');
+	  $(".mobile-menu").fadeToggle();
+
+	});
+
+	$(".exit").click(function(){
+
+		$(this).toggleClass('active');
+		$(".page-nav").fadeToggle();
+	});
+
+	});
+	// end hamburger menu
+
+// for slider
+	$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+});
+
+$('.slider-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
+});
+
+});
